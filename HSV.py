6f214dc69941479481938954df3c06db6afea74a
Leyla TÜLÜ <98leylatulu@gmail.com>
2023-06-05 13:06:26 +0300

    added commits HSV.py

--- HSV.py
+++ HSV.py
@@ -1,8 +1,9 @@
 import cv2
 
-# Bir görüntüyü yükle
+# load image
 img = cv2.imread("yumushan.jpg")
 
+# get image size
 x = img.shape[1]
 y = img.shape[0]
 
@@ -11,7 +12,7 @@ new_x = 450
 new_y = int(new_x * y / x)
 img = cv2.resize(img, (new_x, new_y))
 
-# Görüntüyü HSV renk uzayına dönüştür
+# convert to hsv color space
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
 def click(event, x, y, flags, param):
@@ -20,22 +21,21 @@ def click(event, x, y, flags, param):
         h = hsv[y, x, 0]
         s = hsv[y, x, 1]
         v = hsv[y, x, 2]
-        hsv_uzayi = 'HSV: ' + str(h) + ' ' + str(s) + ' ' + str(v)
-        cv2.putText(img, hsv_uzayi, (x, y), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 255), 2)
+        hsv_space = 'HSV: ' + str(h) + ' ' + str(s) + ' ' + str(v)
+        cv2.putText(img, hsv_space, (x, y), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 255), 2)
         cv2.imshow("Image", img)
 
-
-# Görüntüyü ekranda göster
+# Show image
 cv2.imshow("Image", img)
 
-# Fare olaylarını takip etmek için bir işaretçi ata
+# Assign a pointer to track mouse events
 cv2.setMouseCallback("Image", click)
 
 # ESC tuşuna basılana kadar döngüyü devam ettir
 while True:
     if cv2.waitKey(1) == 27:
         break
-# Pencereyi kapat
+
 cv2.destroyAllWindows()
 
 
